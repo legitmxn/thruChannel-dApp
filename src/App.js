3e2c8970a55385c556790726e426a356fbc7846a
SuperFrank99 <sajaljaiswal5@gmail.com>
2023-02-12 18:53:26 +0530

    created express app; connected to mongodb atlas; created users schema; created insert and get user apis with hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const UserModel = require('../models/user')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const uri = "mongodb+srv://finalyearproject:finalyearprojectpassword@fypcluster.89optaq.mongodb.net/?retryWrites=true&w=majority"
+async function connect(){
+  try{
+    await mongoose.connect(uri);
+    console.log("Connected to Mongodb");
+  } catch(err){
+    console.log(err);
+  }
 }
 
-export default App;
+connect();
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+})
+
+app.get("/insertUser", (req, res) => {
+  let userModel = new UserModel()
+  userModel.name = "Mondeep Prakash"
+  userModel.scholar_id = "1912064"
+  userModel.votes_left = "1000"
+  userModel.club = "MUN"
+  userModel.position = "General Secretary"
+
+  userModel.save((err, data) => {
+    if(err){
+      console.error(err)
+    }else{
+      res.status(200).send({"msg":"Inserted to DB"})
+    }
+  })
+})
+
+app.get("/getUsers", (req,res) => {
+  UserModel.find((err,data) => {
+    if(err){
+      return res.status(500).send(err)
+    }else{
+      return res.status(200).json(data)
+    }
+  })
+})
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
